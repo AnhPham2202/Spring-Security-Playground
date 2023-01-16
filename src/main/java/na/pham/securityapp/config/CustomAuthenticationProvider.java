de80b32599d33aaafb9424e821973db50908dbd5
AnhPham2202 <ngocanhpham220299@gmail.com>
2023-01-16 09:36:21 +0700

    custom auth provider

---
+++ src/main/java/na/pham/securityapp/config/CustomAuthenticationProvider.java
@@ -0,0 +1,21 @@
+package na.pham.securityapp.config;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        System.out.println("Some custom authentication");
+        return authentication;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+    }
+}
--- src/main/java/na/pham/securityapp/config/SecurityConfig.java
+++ src/main/java/na/pham/securityapp/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package na.pham.securityapp.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -11,8 +12,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import javax.sql.DataSource;
+import java.util.Arrays;
 
 @Configuration
 public class SecurityConfig {
@@ -25,7 +30,8 @@ public class SecurityConfig {
                         .and().formLogin()
                         .and().httpBasic()
                 .and()
-                .csrf().disable().build();
+                .csrf().disable()
+                .build();
     }
 
 //    @Bean
